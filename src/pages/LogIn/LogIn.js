25349477dfd53b5cc991937520b397b62949f4c5
dmytropihur <dmytro.pihur@gmail.com>
2022-03-23 23:01:05 +0100

    created log in page and main layout component

--- src/pages/LogIn/LogIn.js
+++ src/pages/LogIn/LogIn.js
@@ -1,22 +1,11 @@
-import { Card, Container, Form } from "react-bootstrap";
 import styled from "@emotion/styled";
-import { Input } from "../../components/Input";
+import { Card, Container, Form } from "react-bootstrap";
 import { Button } from "../../components/Button";
+import { Input } from "../../components/Input";
 
-export const Registration = () => {
+export const LogIn = () => {
+
   const forms = [
-    {
-      label: "Name",
-      name: "name",
-      type: "text",
-      placeholder: "Enter your name",
-    },
-    {
-      label: "Surname",
-      name: "surname",
-      type: "text",
-      placeholder: "Enter your surname",
-    },
     {
       label: "Email",
       name: "email",
@@ -24,22 +13,10 @@ export const Registration = () => {
       placeholder: "Enter your email",
     },
     {
-      label: "Phone",
-      name: "phone",
-      type: "tel",
-      placeholder: "Enter your phone",
-    },
-    {
       label: "Password",
       name: "password",
       type: "password",
       placeholder: "Enter your password",
-    },
-    {
-      label: "Repeat Password",
-      name: "repeatPassword",
-      type: "password",
-      placeholder: "Repeat your password",
     },
   ];
 
@@ -48,7 +25,7 @@ export const Registration = () => {
       <Card style={{ width: "100%" }}>
         <Card.Body>
           <Card.Title as="h2" className="mb-4" style={{ textAlign: "center" }}>
-            Registration
+            Log In
           </Card.Title>
           <Form>
             {forms.map((form) => {
