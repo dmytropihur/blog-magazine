a08abefcbecbde7cfeb876c4d1d58b8db682fa1f
dmytropihur <dmytro.pihur@gmail.com>
2022-04-05 19:39:51 +0200

    realized JWT authentication using redux store and cookies

--- src/pages/LogIn/LogIn.js
+++ src/pages/LogIn/LogIn.js
@@ -1,9 +1,13 @@
+import styled from "@emotion/styled";
+import axios from "axios";
+import { Card, Container, Form } from "react-bootstrap";
 import { useDispatch } from "react-redux";
-import { Form } from "../../components/Form";
 import { useNavigate } from "react-router-dom";
+import { Button } from "../../components/Button";
+import { Input } from "../../components/Input";
 import { userLogin } from "../../store/actionCreators/user.actionCreator";
 
-const fields = [
+const forms = [
   {
     label: "Email",
     name: "email",
@@ -17,26 +21,50 @@ const fields = [
     placeholder: "Enter your password",
   },
 ];
-
 export const LogIn = () => {
+
   const dispatch = useDispatch()
   const navigate = useNavigate()
 
-  const handleSubmit = async ({preventDefault, target}) => {
-    preventDefault();
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const {target} = e
     const formData = new FormData(target)
     const email = formData.get('email')
     const password = formData.get('password')
-
     try {
       dispatch(userLogin({email, password}))
       navigate('/')
+      
     } catch(err) {
       console.log(err);
     }
+
   }
 
+
+
   return (
-    <Form fields={fields} handleSubmit={handleSubmit} title={'Log In'}/>
+    <Box>
+      <Card style={{ width: "100%" }}>
+        <Card.Body>
+          <Card.Title as="h2" className="mb-4" style={{ textAlign: "center" }}>
+            Log In
+          </Card.Title>
+          <Form onSubmit={handleSubmit}>
+            {forms.map((form) => {
+              return <Input props={form} />;
+            })}
+            <Button variant="dark" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+    </Box>
   );
 };
+
+const Box = styled(Container)`
+  max-width: 700px;
+`;
